1e54154cd524d4d679732eb78bf183683dc37587
Sarah A <sera@madotsuki.net>
2023-12-07 16:05:31 +0000

    Changed github workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,5 +11,19 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version "3.16"
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
 include("core")
 
