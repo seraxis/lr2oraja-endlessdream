ded54316bc8ae033bf26d252bebafadc653e72a2
Sarah A <sera@madotsuki.net>
2023-12-27 22:58:41 +0000

    Clarify behaviour of Rate Trainer, code cleanup

--- core/src/bms/player/beatoraja/modmenu/FreqTrainerMenu.java
+++ core/src/bms/player/beatoraja/modmenu/FreqTrainerMenu.java
@@ -5,13 +5,18 @@ import imgui.flag.ImGuiCond;
 import imgui.flag.ImGuiWindowFlags;
 import imgui.type.ImBoolean;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static bms.player.beatoraja.modmenu.ImGuiRenderer.*;
 
 public class FreqTrainerMenu {
 
     public static ImBoolean FREQ_TRAINER_ENABLED = new ImBoolean(false);
 
-    private static int[] freq = new int[] {100};;
+    private static int[] freq = new int[] {100};
+
+    private static List<Integer> buttonVals = Arrays.asList(-10, -5, -1, 100, 1, 5, 10);
 
     public static void show(ImBoolean showFreqTrainer) {
         float relativeX = windowWidth * 0.47f;
@@ -19,47 +24,32 @@ public class FreqTrainerMenu {
         ImGui.setNextWindowPos(relativeX, relativeY, ImGuiCond.FirstUseEver);
 
         if(ImGui.begin("Rate Modifier", showFreqTrainer, ImGuiWindowFlags.AlwaysAutoResize)) {
-            ImGui.text("Set chart playback rate");
-            //ImGui.indent();
+            ImGui.text("Modifies the chart playback rate to be faster or");
+            ImGui.text("slower by a given percent.");
 
-            if(ImGui.button("-10%")) {
-                freq[0] = clamp(freq[0] - 10);
-            }
-            ImGui.sameLine();
-            if(ImGui.button("-5%")) {
-                freq[0] = clamp(freq[0] - 5);
-            }
-            ImGui.sameLine();
-            if(ImGui.button("-1%")) {
-                freq[0] = clamp(freq[0] - 1);
-            }
-            ImGui.sameLine();
-            if(ImGui.button("Reset")) {
-                freq[0] = 100;
-            }
-            ImGui.sameLine();
-            if(ImGui.button("+1%")) {
-                freq[0] = clamp(freq[0] + 1);
-            }
-            ImGui.sameLine();
-            if(ImGui.button("+5%")) {
-                freq[0] = clamp(freq[0] + 5);
-            }
-            ImGui.sameLine();
-            if(ImGui.button("+10%")) {
-                freq[0] = clamp(freq[0] + 10);
-            }
+            buttonVals.forEach(value -> {
+                if (value == 100) {
+                    if(ImGui.button("Reset")) {
+                        freq[0] = 100;
+                    }
+                } else {
+                    if(ImGui.button((value > 0 ? "+" : "") + value + "%")) {
+                        freq[0] = clamp(freq[0] + value);
+                    }
+                }
+                ImGui.sameLine();
+            });
+            ImGui.newLine();
             ImGui.sliderInt("%",
                     freq,
                     50,
                     200);
 
             ImGui.text("Controls");
-
             ImGui.indent();
             ImGui.checkbox("Rate Enabled", FREQ_TRAINER_ENABLED);
             ImGui.sameLine();
-            helpMarker("When enabled scores will save locally, however scores will not submit to IR and result lamp will always be NO PLAY");
+            helpMarker("When enabled positive rate scores will save locally, however scores will not submit to IR and result lamp will always be NO PLAY.");
 
             freq[0] = clamp(freq[0]);
         }
