271059c69fc622e750dc44fc5c0a3c8b05e4e0e5
Catizard <1185032459@qq.com>
2025-10-01 10:20:52 +0800

    store freq & override judge value in separate table

--- core/src/bms/player/beatoraja/ScoreDataLogDatabaseAccessor.java
+++ core/src/bms/player/beatoraja/ScoreDataLogDatabaseAccessor.java
@@ -9,8 +9,12 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.sqlite.SQLiteConfig;
-import org.sqlite.SQLiteDataSource;
 import org.sqlite.SQLiteConfig.SynchronousMode;
+import org.sqlite.SQLiteDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * スコアデータログデータベースアクセサ
@@ -26,7 +30,7 @@ public class ScoreDataLogDatabaseAccessor extends SQLiteDatabaseAccessor {
 	private final QueryRunner qr;
 
 	public ScoreDataLogDatabaseAccessor(String path) throws ClassNotFoundException {
-		super(	new Table("scoredatalog",
+		super( new Table("scoredatalog",
 						new Column("sha256", "TEXT", 1, 1),
 						new Column("mode", "INTEGER",0,1),
 						new Column("clear", "INTEGER"),
@@ -56,7 +60,40 @@ public class ScoreDataLogDatabaseAccessor extends SQLiteDatabaseAccessor {
 						new Column("date", "INTEGER"),
 						new Column("state", "INTEGER"),
 						new Column("scorehash", "TEXT")
-						));
+				),
+				new Table( "eddatalog",
+						new Column("sha256", "TEXT", 1, 0),
+						new Column("mode", "INTEGER"),
+						new Column("clear", "INTEGER"),
+						new Column("epg", "INTEGER"),
+						new Column("lpg", "INTEGER"),
+						new Column("egr", "INTEGER"),
+						new Column("lgr", "INTEGER"),
+						new Column("egd", "INTEGER"),
+						new Column("lgd", "INTEGER"),
+						new Column("ebd", "INTEGER"),
+						new Column("lbd", "INTEGER"),
+						new Column("epr", "INTEGER"),
+						new Column("lpr", "INTEGER"),
+						new Column("ems", "INTEGER"),
+						new Column("lms", "INTEGER"),
+						new Column("notes", "INTEGER"),
+						new Column("combo", "INTEGER"),
+						new Column("minbp", "INTEGER"),
+						new Column("avgjudge", "INTEGER", 1, 0, String.valueOf(Integer.MAX_VALUE)),
+						new Column("playcount", "INTEGER"),
+						new Column("clearcount", "INTEGER"),
+						new Column("trophy", "TEXT"),
+						new Column("ghost", "TEXT"),
+						new Column("option", "INTEGER"),
+						new Column("seed", "INTEGER"),
+						new Column("random", "INTEGER"),
+						new Column("date", "INTEGER"),
+						new Column("state", "INTEGER"),
+						new Column("scorehash", "TEXT"),
+						new Column("rate", "INTEGER"),
+						new Column("overridejudge", "INTEGER")
+				));
 
 		Class.forName("org.sqlite.JDBC");
 		SQLiteConfig conf = new SQLiteConfig();
@@ -83,6 +120,7 @@ public class ScoreDataLogDatabaseAccessor extends SQLiteDatabaseAccessor {
 			con.setAutoCommit(false);
 			for (ScoreData score : scores) {
 				this.insert(qr, con, "scoredatalog", score);
+				this.insert(qr, con, "eddatalog", score);
 			}
 			con.commit();
 		} catch (Exception e) {
