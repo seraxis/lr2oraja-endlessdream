cc7a6378049fe700a48dbf20015156dbe69a0cf3
dgarciah98 <dieg0farrobaterra@gmail.com>
2025-11-09 21:33:43 +0100

    fix: reload SkinMenu to persist skin change

--- core/src/bms/player/beatoraja/MainController.java
+++ core/src/bms/player/beatoraja/MainController.java
@@ -336,6 +336,7 @@ public class MainController {
 		playdata = new PlayDataAccessor(config);
 
 	  initializeIRConfig();
+    SkinMenu.init(this, player);
 
     loudnessAnalyzer = new BMSLoudnessAnalyzer(config);
 		resource = new PlayerResource(audio, config, pc, loudnessAnalyzer);
