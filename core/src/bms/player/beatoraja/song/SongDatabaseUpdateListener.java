05b0873e8aa0bd0e1ad796bc42b9cb7ade9594d8
Catizard <Salieri_XY@163.com>
2025-11-18 19:59:11 +0800

    Show current songdata.db building progress

---
+++ core/src/bms/player/beatoraja/song/SongDatabaseUpdateListener.java
@@ -0,0 +1,38 @@
+package bms.player.beatoraja.song;
+
+import javafx.beans.property.IntegerProperty;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Listen current songdata.db's update progress
+ */
+public class SongDatabaseUpdateListener {
+	private final AtomicInteger bmsFiles = new AtomicInteger(0);
+	private final AtomicInteger skipBMSFiles = new AtomicInteger(0);
+	private final AtomicInteger newBMSFiles = new AtomicInteger(0);
+
+	public void addBMSFilesCount(int count) {
+		bmsFiles.addAndGet(count);
+	}
+
+	public void addSkipBMSFilesCount(int count) {
+		skipBMSFiles.addAndGet(count);
+	}
+
+	public void addNewBMSFilesCount(int count) {
+		newBMSFiles.addAndGet(count);
+	}
+
+	public int getBMSFilesCount() {
+		return bmsFiles.get();
+	}
+
+	public int getSkipBMSFilesCount() {
+		return skipBMSFiles.get();
+	}
+
+	public int getNewBMSFilesCount() {
+		return newBMSFiles.get();
+	}
+}
