f83516105ff483ab34278cf31514511dde2c3188
arc <naurthal0@gmail.com>
2025-11-20 13:55:55 +0100

    hide batch downloading when http disabled, course batch download
    # Conflicts:
    #	core/src/bms/player/beatoraja/select/MusicSelector.java

--- core/src/bms/player/beatoraja/select/MusicSelector.java
+++ core/src/bms/player/beatoraja/select/MusicSelector.java
@@ -275,7 +275,7 @@ public final class MusicSelector extends MainState {
 				} else if (song.getIpfs() != null && main.getMusicDownloadProcessor() != null
 						&& main.getMusicDownloadProcessor().isAlive()) {
 					execute(MusicSelectCommand.DOWNLOAD_IPFS);
-				} else if (main.getHttpDownloadProcessor() != null && main.getHttpDownloadProcessor() != null) {
+				} else if (main.getHttpDownloadProcessor() != null) {
 					execute(MusicSelectCommand.DOWNLOAD_HTTP);
 				} else {
 	                executeEvent(EventType.open_download_site);
@@ -448,6 +448,9 @@ public final class MusicSelector extends MainState {
 		final GradeBar gradeBar = (GradeBar) manager.getSelected();
 		if (!gradeBar.existsAllSongs()) {
 			logger.info("段位の楽曲が揃っていません");
+            if (main.getHttpDownloadProcessor() != null) {
+                execute(MusicSelectCommand.DOWNLOAD_COURSE_HTTP);
+            }
 			return;
 		}
 
