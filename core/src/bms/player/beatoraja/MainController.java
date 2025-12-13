ca69f3937ceb1cd419b554f4ac1b259e8bc5a98e
Catizard <Salieri_XY@163.com>
2025-12-13 10:27:21 +0800

    Allow user loads skin in legacy mode

--- core/src/bms/player/beatoraja/MainController.java
+++ core/src/bms/player/beatoraja/MainController.java
@@ -137,6 +137,8 @@ public class MainController {
 
 	public List<IRSendStatus> irSendStatus = new ArrayList<IRSendStatus>();
 
+	private boolean legacySkinMode = false;
+
 	public MainController(Path f, Config config, PlayerConfig player, BMSPlayerMode auto, boolean songUpdated) {
 		this.auto = auto;
 		this.config = config;
@@ -1016,6 +1018,14 @@ public class MainController {
 
 	public void setMicroTimer(int id, long microtime) {
 		timer.setMicroTimer(id, microtime);
+	}
+
+	public boolean isLegacySkinMode() {
+		return legacySkinMode;
+	}
+
+	public void setLegacySkinMode(boolean legacySkinMode) {
+		this.legacySkinMode = legacySkinMode;
 	}
 
 	public HttpDownloadProcessor getHttpDownloadProcessor() {
