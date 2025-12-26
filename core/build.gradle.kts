f6da96d9ed518e99483896cb1d0ef11a78ec61fb
Catizard <Salieri_XY@163.com>
2025-12-26 12:33:02 +0800

    Replace jbmstable-parser with kbmstable-parser

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -31,7 +31,7 @@ version = libs.versions.beatoraja.get()
 
 sourceSets {
     main {
-        java.srcDirs(listOf("src/", "dependencies/jbms-parser/", "dependencies/jbmstable-parser"))
+        java.srcDirs(listOf("src/", "dependencies/jbms-parser/"))
         resources.srcDirs(listOf("src/"))
     }
 }
@@ -171,6 +171,9 @@ dependencies {
 
     implementation(libs.javawebsocket)
     implementation(libs.bundles.slf4j)
+
+    implementation(libs.kotlin.reflect)
+    implementation(libs.kbmstableparser)
 
     // non-gradle managed file dependencies. jportaudio not on maven. "custom" scares me.
     implementation(":jportaudio")
