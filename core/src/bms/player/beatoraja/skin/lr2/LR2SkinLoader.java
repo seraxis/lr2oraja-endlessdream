e232bda93de55d2cbe365b87d38dbdde28b25076
Catizard <Salieri_XY@163.com>
2026-03-07 11:16:45 +0800

    Rewrite the #IF/#ELSE/#ELSEIF handle strategy to correctly parse nested statements

--- core/src/bms/player/beatoraja/skin/lr2/LR2SkinLoader.java
+++ core/src/bms/player/beatoraja/skin/lr2/LR2SkinLoader.java
@@ -1,6 +1,8 @@
 package bms.player.beatoraja.skin.lr2;
 
 import java.io.File;
+import java.util.Deque;
+import java.util.LinkedList;
 
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.IntIntMap;
@@ -9,6 +11,7 @@ import com.badlogic.gdx.utils.ObjectMap;
 import bms.player.beatoraja.MainState;
 import bms.player.beatoraja.skin.SkinLoader;
 import bms.player.beatoraja.skin.property.*;
+import com.badlogic.gdx.utils.Queue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,101 +43,93 @@ public abstract class LR2SkinLoader extends SkinLoader {
 			return;
 		}
 		String[] str = line.split(",", -1);
-		if (str.length > 0) {
-			if (str[0].equalsIgnoreCase("#IF")) {
-				ctx.insideIf = true;
-				for (int i = 1; i < str.length; i++) {
-					boolean b = false;
-					if (str[i].length() == 0) {
-						continue;
-					}
-					try {
-						int opt = Integer.parseInt(str[i].replace('!', '-').replaceAll("[^0-9-]", ""));
-						if(opt >=  0) {
-							if(op.get(opt, -1) == 1) {
-								b = true;
-							}
-						} else {
-							if(op.get(-opt, -1) == 0) {
-								b = true;
-							}
-						}
-						if (!b && !op.containsKey(Math.abs(opt)) && state != null) {
-							BooleanProperty draw = BooleanPropertyFactory.getBooleanProperty(opt);
-							if(draw != null) {
-								b = draw.get(state);								
-							}
-						}
-						if (!b) {
-							ctx.insideIf = false;
-							break;
-						}
-					} catch (NumberFormatException e) {
-						e.printStackTrace();
-						break;
-					}
+		if (str.length == 0) {
+			return ;
+		}
+		String head = str[0].toUpperCase();
+		switch (head) {
+			case "#IF" -> {
+				boolean ok = true;
+				for (int i = 1; i < str.length; ++i) {
+					ok &= parseOneCond(str[i], state);
 				}
-
-				ctx.skip = !ctx.insideIf;
-			} else if (str[0].equalsIgnoreCase("#ELSEIF")) {
-				if (ctx.insideIf) {
-					ctx.skip = true;
-				} else {
-					ctx.insideIf = true;
-					for (int i = 1; i < str.length; i++) {
-						boolean b = false;
-						try {
-							int opt = Integer.parseInt(str[i].replace('!', '-').replaceAll("[^0-9-]", ""));
-							if(opt >=  0) {
-								if(op.get(opt, -1) == 1) {
-									b = true;
-								}
-							} else {
-								if(op.get(-opt, -1) == 0) {
-									b = true;
-								}
-							}
-							if (!b && !op.containsKey(Math.abs(opt)) && state != null) {
-								BooleanProperty draw = BooleanPropertyFactory.getBooleanProperty(opt);
-								if(draw != null) {
-									b = draw.get(state);								
-								}
-							}
-							if (!b) {
-								ctx.insideIf = false;
-								break;
-							}
-						} catch (NumberFormatException e) {
-							break;
-						}
-					}
-
-					ctx.skip = !ctx.insideIf;
-				}
-			} else if (str[0].equalsIgnoreCase("#ELSE")) {
-				ctx.skip = ctx.insideIf;
-			} else if (str[0].equalsIgnoreCase("#ENDIF")) {
-				ctx.skip = false;
-				ctx.insideIf = false;
+				ctx.push(ok);
 			}
-			if (!ctx.skip) {
-				if (str[0].equalsIgnoreCase("#SETOPTION")) {
-					int index = Integer.parseInt(str[1]);
-					op.put(index, Integer.parseInt(str[2]) >= 1 ? 1 : 0);
-				}
-
-				Command command = null;
-				for (Command cm : commands) {
-					if (str[0].substring(1).equalsIgnoreCase(cm.name())) {
-						command = cm;
-						break;
+			case "#ELSEIF" -> {
+				Context.State top = ctx.top();
+				if (top != null) {
+					if (top.matched) {
+						top.active = false;
+					} else {
+						boolean ok = true;
+						for (int i = 1; i < str.length; i++) {
+							ok &= parseOneCond(str[i], state);
+						}
+						top.matched = ok;
+						top.active = ok;
 					}
 				}
-				if (command != null) {
-					command.execute(this, str);
+			}
+			case "#ELSE" -> {
+				Context.State top = ctx.top();
+				if (top != null) {
+					if (top.matched) {
+						top.active = false;
+					} else {
+						top.matched = true;
+						top.active = true;
+					}
+				}
+			}
+			case "#ENDIF" -> ctx.pop();
+			default -> {
+				Context.State top = ctx.top();
+				if (top == null || top.active) {
+					if (str[0].equalsIgnoreCase("#SETOPTION")) {
+						int index = Integer.parseInt(str[1]);
+						op.put(index, Integer.parseInt(str[2]) >= 1 ? 1 : 0);
+					}
+
+					Command command = null;
+					for (Command cm : commands) {
+						if (str[0].substring(1).equalsIgnoreCase(cm.name())) {
+							command = cm;
+							break;
+						}
+					}
+					if (command != null) {
+						command.execute(this, str);
+					}
 				}
 			}
 		}
+	}
+
+	protected boolean parseOneCond(String cond, MainState state) {
+		if (cond == null || cond.isEmpty())	{
+			return true;
+		}
+		try {
+			int opt = Integer.parseInt(cond.replace('!', '-').replaceAll("[^0-9-]", ""));
+			if (opt >= 0) {
+				if (op.get(opt, -1) == 1) {
+					return true;
+				}
+			} else {
+				if (op.get(-opt, -1) == 0) {
+					return true;
+				}
+			}
+			if (!op.containsKey(Math.abs(opt)) && state != null) {
+				BooleanProperty draw = BooleanPropertyFactory.getBooleanProperty(opt);
+				if (draw != null) {
+					return draw.get(state);
+				}
+			}
+		} catch (NumberFormatException e) {
+			logger.warn("Failed to parse a condition: ", e);
+		}
+		return true;
 	}
 
 	public IntIntMap getOption() {
@@ -190,17 +185,13 @@ public abstract class LR2SkinLoader extends SkinLoader {
 
 	/**
 	 * Context of loading a lr2 skin
+	 *
+	 * @implNote Dangling states like isolatede #ELSE is being ignored. E.g. dangling #ELSE will be executed no matter
+	 *  how, same as dangling #ELSEIF. The very one exception is a #IF command without #ENDIF is going to be persisted
+	 *  in the stack and anything could happen after.
 	 */
 	protected static class Context {
-		/**
-		 * Whether do we need to skip the next lines
-		 */
-		public boolean skip = false;
-
-		/**
-		 * Whether are we inside an if statement
-		 */
-		public boolean insideIf = false;
+		private Deque<State> stack;
 
 		/**
 		 * Skin config options
@@ -209,6 +200,36 @@ public abstract class LR2SkinLoader extends SkinLoader {
 
 		public Context(IntIntMap op) {
 			this.op = op;
+			stack = new LinkedList<>();
+		}
+
+		public void push(boolean cond) {
+			stack.push(new State(cond, cond));
+		}
+
+		public void pop() {
+			if (!stack.isEmpty()) {
+				stack.pop();
+			}
+		}
+
+		public State top() {
+			return stack.isEmpty() ? null : stack.getLast();
+		}
+
+		/**
+		 * A simple pair of boolean:
+		 *  matched: whether there's a branch that has been matched
+		 *  active: whether we're in an 'active' branch (we need to execute the lines within) or not
+		 */
+		public static class State {
+			public boolean matched;
+			public boolean active;
+
+			public State(boolean matched, boolean active) {
+				this.matched = matched;
+				this.active = active;
+			}
 		}
 	}
 }
